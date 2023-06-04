92a74af1df8e60cc93feed4775506a1ac6fd92e5
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-06-04 17:53:30 -0400

    Complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function handleBlur(event){
+    console.log("Hey! Eyes on me!")
+}
+
+function handleFocus(event){
+    console.log("Good!")
+}
+
+function EyesOnMe (){
+    return(
+    <div>
+        <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+    </div>
+    )
+}
+
+export default EyesOnMe
